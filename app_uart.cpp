9ea3ef4e1d5b880bade07630401c4dde8ea8e921
KisonHe <kisonmaxine@gmail.com>
2020-12-20 23:58:18 +0800

    Init

---
+++ app_uart.cpp
@@ -0,0 +1,21 @@
+/**
+ * @file app_uart.cpp
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2020-12-20
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "esp_log.h"
+#include "driver/gpio.h"
+
+#include "app_uart.h"
---
+++ include/app_uart.h
@@ -0,0 +1,15 @@
+/**
+ * @file app_uart.h
+ * @author kisonhe
+ * @brief 
+ * @version 0.1
+ * @date 2020-12-20
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+#ifndef APP_UART_H
+#define APP_UART_H
+
+
+#endif
